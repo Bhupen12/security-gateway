849447cdd9ae46849d14f4aa0efd0b3a45553bd6
BhupenSahoo-Suraksha <160491383+BhupenSahoo-Suraksha@users.noreply.github.com>
2025-11-21 09:46:12 +0530

    updated .gitignore

--- packages/core/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SecurityGateway = void 0;
-class SecurityGateway {
-    constructor() {
-        console.log("✅ Security Gateway initialized from Core!");
-    }
-}
-exports.SecurityGateway = SecurityGateway;
